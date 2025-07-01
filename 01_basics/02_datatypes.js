a536236ddc2711f4bef7d62d1bbb844c3f623a96
doowoop <162444849+doowoop@users.noreply.github.com>
2025-07-01 14:32:04 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// alert(3+3) //using node js, not browser
+
+console.log(3
+    + 3
+); //code readbility should be high
+//tc39 ecma documentation official
+//mdn documentation unofficial
+
+let name = "utsav"
+let age = 21
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigint for large number
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object 
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
