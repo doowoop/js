aa7bea9efdb12a28a8ad6dc99baa95a4609a05fe
doowoop <162444849+doowoop@users.noreply.github.com>
2025-07-01 19:34:32 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(1 < 2);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0); // false
+// console.log(null == 0); // false
+// console.log(null >= 0); // true because "<" or ">" (but not "=") treat null as a number (or 0) so 0 >= 0 and 0 > 0 return false
+
+// console.log(undefined == 0); // false
+// console.log(undefined > 0); // false
+// console.log(undefined < 0); // false
+
+// strict check ===
+
+console.log("2" === 2); // false
