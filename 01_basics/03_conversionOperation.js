8daf6b8d5965ffe2bf268aba96bda2bde9abba3c
doowoop <162444849+doowoop@users.noreply.github.com>
2025-07-10 15:43:57 +0000

    function

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -19,8 +19,51 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // "utsav" => true;
 // "" => false
 
-let someNumber = 33
+ let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+ let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *************** Operations ***************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " utsav"
+
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2) // 122
+// console.log(1 + 2 + "2") // (1+2) + "2" = 32
+// If string appears fisrt then treats it as string if number appears then treats as number
+
+// console.log(+true); // 1
+// console.log(+""); // 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
